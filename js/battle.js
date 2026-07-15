7e23c2c260d4afae40c2eb39f033c3fd0dcf08cc
panda3123-lgtm <panda3.123@icloud.com>
2026-07-15 22:05:39 +0900

    Update battle.js

--- js/battle.js
+++ js/battle.js
@@ -4,6 +4,47 @@
 // ===============================
 
 
+let cards = [];
+
+
+
+// cards.json読み込み
+
+fetch("cards.json")
+
+.then(response => response.json())
+
+.then(data => {
+
+    cards = data;
+
+    updateHand();
+
+});
+
+
+
+
+
+
+// カード情報取得
+
+function getCardData(name){
+
+
+    return cards.find(
+
+        card => card.name === name
+
+    );
+
+
+}
+
+
+
+
+
 
 
 
@@ -16,6 +57,9 @@ function updateHand(){
     document.getElementById(
         "hand-zone"
     );
+
+
+    if(!handZone) return;
 
 
 
@@ -32,7 +76,7 @@ function updateHand(){
 
 
         button.textContent =
-        card;
+        card.name ?? card;
 
 
 
@@ -61,15 +105,23 @@ function updateHand(){
 
 // カード使用
 
-function playCard(cardName){
-
-
-    const index =
-    player.hand.indexOf(cardName);
+function playCard(card){
 
 
 
-    if(index === -1){
+    const cardData =
+    typeof card === "string"
+    ? getCardData(card)
+    : card;
+
+
+
+    if(!cardData){
+
+        console.log(
+            "カード情報なし",
+            card
+        );
 
         return;
 
@@ -77,16 +129,9 @@ function playCard(cardName){
 
 
 
-    // カードデータ取得予定
 
-    const card =
-    getCardData(cardName);
+    if(player.cost < cardData.cost){
 
-
-
-    if(
-        player.cost < card.cost
-    ){
 
         alert(
             "コスト不足"
@@ -100,26 +145,38 @@ function playCard(cardName){
 
 
 
-    player.cost -= card.cost;
+    player.cost -= cardData.cost;
 
 
 
-    player.hand.splice(
-        index,
-        1
-    );
+    // 手札から削除
+
+    const index =
+    player.hand.indexOf(card);
+
+
+
+    if(index !== -1){
+
+        player.hand.splice(
+            index,
+            1
+        );
+
+    }
 
 
 
 
 
 
-    if(card.type === "monster"){
+
+    if(cardData.type === "monster"){
 
 
         summonMonster(
             player,
-            card
+            cardData
         );
 
 
@@ -127,12 +184,11 @@ function playCard(cardName){
 
 
 
-    else if(card.type === "magic"){
+    else if(cardData.type === "magic"){
 
 
         activateMagic(
-            player,
-            card
+            cardData
         );
 
 
@@ -140,18 +196,21 @@ function playCard(cardName){
 
 
 
-    else if(card.type === "trap"){
+    else if(cardData.type === "trap"){
 
 
         setTrap(
             player,
-            card
+            cardData
         );
 
 
     }
 
 
+
+
+    updateHand();
 
     updateScreen();
 
@@ -168,41 +227,58 @@ function playCard(cardName){
 
 // モンスター召喚
 
-function summonMonster(target,card){
-
-
-    target.field.push(card);
+function summonMonster(player,card){
 
 
 
-    if(target === player){
-
-
-        const zone =
-        document.getElementById(
-            "player-monster-zone"
-        );
+    player.field.push(card);
 
 
 
-        const cardView =
-        document.createElement(
-            "button"
-        );
-
-
-        cardView.textContent =
-        card.name;
+    const zone =
+    document.getElementById(
+        "player-monster-zone"
+    );
 
 
 
-        zone.appendChild(
-            cardView
-        );
+    const view =
+    document.createElement("div");
 
 
-    }
 
+    view.textContent =
+    card.name;
+
+
+
+    zone.appendChild(view);
+
+
+}
+
+
+
+
+
+
+
+
+
+// 魔法発動
+
+function activateMagic(card){
+
+
+
+    console.log(
+        card.name+
+        "を発動"
+    );
+
+
+
+    // 後でeffect.jsへ
 
 }
 
@@ -216,81 +292,18 @@ function summonMonster(target,card){
 
 // トラップセット
 
-function setTrap(target,card){
+function setTrap(player,card){
 
 
-    target.trap.push(card);
+
+    player.trap.push(card);
 
 
-    alert(
+
+    console.log(
         card.name+
-        "をセットしました"
+        "をセット"
     );
 
 
 }
-
-
-
-
-
-
-
-
-// 魔法発動
-
-function activateMagic(target,card){
-
-
-    activateEffect(
-        card.name
-    );
-
-
-}
-
-
-
-
-
-
-
-
-// 攻撃
-
-function attack(attacker){
-
-
-    // 後で実装
-
-}
-
-
-
-
-
-
-// ターン終了
-
-document
-.getElementById("end-turn")
-.onclick=function(){
-
-
-    if(turnPlayer==="player"){
-
-
-        turnPlayer="enemy";
-
-
-    }
-    else{
-
-
-        turnPlayer="player";
-
-
-    }
-
-
-};
